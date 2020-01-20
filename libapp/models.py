050ab7c0b2edc36180ae4d315957001ff5e77b48
dan <dan@localhost.localdomain>
2020-01-20 17:18:57 +0300

    this creates a class model that carries the rating options

--- libapp/models.py
+++ libapp/models.py
@@ -10,7 +10,17 @@ class Books(models.Model):
     author = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     description = HTMLField()
+    published_date = models.DateField
 
-
+class Rating(models.Model):
+    '''
+    this class defines the rating of the books
+    '''
+    content = models.IntegerField(default=1)
+    theme = models.IntegerField(default = 1)
+    physical_appearance = models.IntegerField(default  = 1)
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    book_id = models.ForeignKey(Books,on_delete = models.CASCADE)
+    
 
 # Create your models here.
