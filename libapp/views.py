fbfb8f9fe451375f4991e7cb6e6a8ef4d73ff826
dan <dan@localhost.localdomain>
2020-01-20 16:24:42 +0300

    create the authentication feature

---
+++ libapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+
+        if password1 == password:
+            if User.objects.filter(username=username):
+                messages.info(request, 'This Username is taken!')
+                return redirect('registration')
+            elif User.objects.filter(email=email):
+                messages.info(request, 'This Email is taken!')
+                return redirect('registration')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)   
+                user.save()           
+                
+                return redirect('home')
+        else:
+            messages.info(request,'Passwords should match!')
+            return redirect('registration')
+    else:
+        return render(request,'auth/registration.html')
+@login_required()
+def home(request):
+    return render(request,'all/home.html')
