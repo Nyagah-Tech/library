ac535ea633375d9d493679e2e3265688ad7d06a6
dan <dan@localhost.localdomain>
2020-01-20 16:58:00 +0300

    create books model class

--- libapp/models.py
+++ libapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
+class Books(models.Model):
+    '''
+    this is a class that defines the structure in which a book will take to be saved in a db
+    '''
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    description = HTMLField()
+
+
 
 # Create your models here.
