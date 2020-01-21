47718e991b5797d348da16fee2b35d9aabe9ec48
dan <dan@localhost.localdomain>
2020-01-21 21:37:28 +0300

    create the dashboard urls

--- libapp/urls.py
+++ libapp/urls.py
@@ -6,5 +6,15 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.home, name="home"),
     path('view/book/<int:id>',views.borrow_view,name="borrow"),
+    path('books/category/<int:id>',views.category_view,name='category'),
+    #dashboard
+    path('dashboard/',views.dashboard,name="user_dashboard"),
+    path('users/',views.registered_users,name="system_users"),
+    path("user/activate/<int:id>",views.user_activate,name="activate_user"),
+    path("user/deactivate/<int:id>",views.user_deactivate,name="deactivate_user")
+    
     
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
