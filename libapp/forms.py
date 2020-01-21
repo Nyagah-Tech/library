13673536bd7775b94d0f85e549552ff031973e88
dan <dan@localhost.localdomain>
2020-01-21 12:35:04 +0300

    create the borrow form

--- libapp/forms.py
+++ libapp/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import *
 
-class Books(forms.ModelForm):
+class BooksForm(forms.ModelForm):
     class Meta:
         model = Books
         fields = [
@@ -13,3 +13,15 @@ class Books(forms.ModelForm):
             'published_date',
             'fee',
         ]
+
+class borrowform(forms.ModelForm):
+    class Meta:
+        model = Borrowing
+        exclude = {
+            'book_id',
+            'user_id',
+            'borrowed_on',
+            'penalty',
+            'total_fee',
+        }
+            
