91b5b3ad4da2173b76fc45cab5549e378239ef36
dan <dan@localhost.localdomain>
2020-01-22 12:32:42 +0300

    create the path to the urls paypal functionality

--- libapp/urls.py
+++ libapp/urls.py
@@ -1,17 +1,21 @@
 from django.conf import settings
-from django.urls import path
+from django.urls import path,include
 from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name="home"),
-    path('view/book/<int:id>',views.borrow_view,name="borrow"),
+    path('view/book/<int:book_id>',views.borrow_view,name="borrow"),
     path('books/category/<int:id>',views.category_view,name='category'),
     #dashboard
     path('dashboard/',views.dashboard,name="user_dashboard"),
     path('users/',views.registered_users,name="system_users"),
     path("user/activate/<int:id>",views.user_activate,name="activate_user"),
-    path("user/deactivate/<int:id>",views.user_deactivate,name="deactivate_user")
+    path("user/deactivate/<int:id>",views.user_deactivate,name="deactivate_user"),
+    path("add/book",views.add_book,name="add-book"),
+    #paypal
+    path("paypal/",include('paypal.standard.ipn.urls')),
+    path("process-payment/",views.process_payment,name="process_payment"),
     
     
 ]
