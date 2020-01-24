cc74c30ad11d61d45c249f1d433a4aeb36a68da1
dan <dan@localhost.localdomain>
2020-01-24 14:22:56 +0300

    fix

--- library/settings.py
+++ library/settings.py
@@ -177,6 +177,7 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
